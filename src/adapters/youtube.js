cb422caca4072dad0836515cd80f597cf156c538
Pere Ayats Marsal <pereayats@Peres-MacBook-Pro.local>
2023-10-30 17:27:07 +0100

    fixing cron

--- src/adapters/youtube.js
+++ src/adapters/youtube.js
@@ -1,155 +1,159 @@
 require('dotenv').config()
 
 const moment = require('moment')
+const axios = require('axios')
 
 const BASE_API_URL = 'https://youtube.googleapis.com/youtube/v3'
 
 function getChannelByVideoId(videoId, callback) {
-    fetch(`${BASE_API_URL}/videos?id=${videoId}&part=snippet&key=${process.env.YOUTUBE_API_KEY}`)
-    .then(response => {
-        if (response.ok) return response.json()
-        else throw new Error('Could not complete request')
+    axios.get(`${BASE_API_URL}/videos`, {
+        params: {
+            id: videoId,
+            part: 'snippet',
+            key: process.env.YOUTUBE_API_KEY,
+        },
     })
-    .then(data => {
-        if (data && data.items && data.items.length > 0) {
-            callback(null, data.items[0].snippet.channelId)
-        }
-        else callback('Video not found')
-    })
-    .catch(error => {
-        callback(error)
-    }) 
+        .then(response => {
+            if (response.data && response.data.items && response.data.items.length > 0) {
+                callback(null, response.data.items[0].snippet.channelId);
+            } else {
+                callback('Video not found');
+            }
+        })
+        .catch(error => {
+            callback(error);
+        });
 }
 
 function getChannelById(channelId, callback) {
-    fetch(`${BASE_API_URL}/channels?id=${channelId}&part=snippet,contentDetails&key=${process.env.YOUTUBE_API_KEY}`)
-    .then(response => {
-        if (response.ok) return response.json()
-        else throw new Error('Could not complete request')
+    axios.get(`${BASE_API_URL}/channels`, {
+        params: {
+            id: channelId,
+            part: 'snippet,contentDetails',
+            key: process.env.YOUTUBE_API_KEY,
+        },
     })
-    .then(data => {
-        if (data && data.items && data.items.length > 0) {
-            const channel = {
-                id: data?.items[0]?.id,
-                title: data?.items[0]?.snippet?.title,
-                description: data?.items[0]?.snippet?.description,
-                customUrl: data?.items[0]?.snippet?.customUrl,
-                publishedAt: data?.items[0]?.snippet?.publishedAt,
-                thumbnail: data?.items[0]?.snippet?.thumbnails?.default?.url,
-                country: data?.items[0]?.snippet?.country,
-                uploadsPlaylistId: data?.items[0]?.contentDetails?.relatedPlaylists?.uploads,
-            }
+        .then(response => {
+            if (response.data && response.data.items && response.data.items.length > 0) {
+                const channelData = response.data.items[0].snippet;
+                const contentDetails = response.data.items[0].contentDetails;
+                const channel = {
+                    id: channelId,
+                    title: channelData.title,
+                    description: channelData.description,
+                    customUrl: channelData.customUrl,
+                    publishedAt: channelData.publishedAt,
+                    thumbnail: channelData.thumbnails.default.url,
+                    country: channelData.country,
+                    uploadsPlaylistId: contentDetails.relatedPlaylists.uploads,
+                };
 
-            callback(null, channel)
-        }
-        else callback('Channel not found')
-    })
-    .catch(error => {
-        callback(error)
-    }) 
+                callback(null, channel);
+            } else {
+                callback('Channel not found');
+            }
+        })
+        .catch(error => {
+            callback(error);
+        });
 }
 
 function getVideosByPlaylistId(playlistId, callback) {
-    fetch(`${BASE_API_URL}/playlistItems?playlistId=${playlistId}&part=contentDetails&key=${process.env.YOUTUBE_API_KEY}&maxResults=50`)
-    .then(response => {
-        if (response.ok) return response.json()
-        else throw new Error('Could not complete request')
+    axios.get(`${BASE_API_URL}/playlistItems`, {
+        params: {
+            playlistId: playlistId,
+            part: 'contentDetails',
+            key: process.env.YOUTUBE_API_KEY,
+            maxResults: 50,
+        },
     })
-    .then(data => {
-        if (data && data.items && data.items.length > 0) {
-            callback(null, data.items.map(v => v?.contentDetails?.videoId))
-        }
-        else callback('Playlist not found')
-    })
-    .catch(error => {
-        callback(error)
-    }) 
+        .then(response => {
+            if (response.data && response.data.items && response.data.items.length > 0) {
+                const videoIds = response.data.items.map(item => item?.contentDetails?.videoId);
+                callback(null, videoIds);
+            } else {
+                callback('Playlist not found');
+            }
+        })
+        .catch(error => {
+            callback(error);
+        });
 }
 
 function getChannelsInfo(channelIds, callback) {
-    let url = `${BASE_API_URL}/channels?part=snippet,contentDetails&key=${process.env.YOUTUBE_API_KEY}`
+    let url = `${BASE_API_URL}/channels`;
+    const params = {
+        part: 'snippet,contentDetails',
+        key: process.env.YOUTUBE_API_KEY,
+        id: channelIds.join(','),
+    };
 
-    channelIds.forEach((id, i) => {
-        if (i == 0) url += `&id=${id}`
-        else url += `,${id}`
-    })
+    axios.get(url, { params })
+        .then(response => {
+            if (response.data && response.data.items && response.data.items.length > 0) {
+                const channels = response.data.items.map(channel => ({
+                    id: channel.id,
+                    title: channel.snippet.title,
+                    description: channel.snippet.description,
+                    customUrl: channel.snippet.customUrl,
+                    publishedAt: channel.snippet.publishedAt,
+                    thumbnail: channel.snippet.thumbnails.default.url,
+                    country: channel.snippet.country,
+                    uploadsPlaylistId: channel.contentDetails.relatedPlaylists.uploads,
+                }));
 
-    fetch(url)
-    .then(response => {
-        if (response.ok) return response.json()
-        else throw new Error('Could not complete request')
-    })
-    .then(data => {
-        if (data && data.items && data.items.length > 0) {
-            const channels = []
-
-            data.items.forEach(channel => {
-                channels.push({
-                    id: channel?.id,
-                    title: channel?.snippet?.title,
-                    description: channel?.snippet?.description,
-                    customUrl: channel?.snippet?.customUrl,
-                    publishedAt: channel?.snippet?.publishedAt,
-                    thumbnail: channel?.snippet?.thumbnails?.default?.url,
-                    country: channel?.snippet?.country,
-                    uploadsPlaylistId: channel?.contentDetails?.relatedPlaylists?.uploads,
-                })
-            })
-
-            callback(null, channels)
-        }
-        else callback('Channels not found')
-    })
-    .catch(error => {
-        callback(error)
-    }) 
+                callback(null, channels);
+            } else {
+                callback('Channels not found');
+            }
+        })
+        .catch(error => {
+            callback(error);
+        });
 }
 
 function getVideosStats(videoIds, callback) {
-    let url = `${BASE_API_URL}/videos?part=contentDetails,id,liveStreamingDetails,snippet,statistics,status,topicDetails&key=${process.env.YOUTUBE_API_KEY}`
+    let url = `${BASE_API_URL}/videos`;
+    const params = {
+        part: 'contentDetails,id,liveStreamingDetails,snippet,statistics,status,topicDetails',
+        key: process.env.YOUTUBE_API_KEY,
+        id: videoIds.join(','),
+    };
 
-    videoIds.forEach((id, i) => {
-        if (i == 0) url += `&id=${id}`
-        else url += `,${id}`
-    })
+    axios.get(url, { params })
+        .then(response => {
+            if (response.data && response.data.items && response.data.items.length > 0) {
+                const videos = response.data.items
+                    .filter(video => !video.liveStreamingDetails)
+                    .map(video => {
+                        const duration = moment.duration(video.contentDetails.duration);
 
-    fetch(url)
-    .then(response => {
-        if (response.ok) return response.json()
-        else throw new Error('Could not complete request')
-    })
-    .then(data => {
-        if (data && data.items && data.items.length > 0) {
-            const videos = []
+                        if (duration.asMilliseconds() > 60 * 1000) {
+                            return {
+                                id: video.id,
+                                channelId: video.snippet.channelId,
+                                title: video.snippet.title,
+                                description: video.snippet.description,
+                                publishedAt: video.snippet.publishedAt,
+                                thumbnail: video.snippet.thumbnails.default.url,
+                                duration: duration.as('seconds'),
+                                views: Number(video.statistics.viewCount) || 0,
+                                likes: Number(video.statistics.likeCount) || 0,
+                                comments: Number(video.statistics.commentCount) || 0,
+                            };
+                        }
 
-            data.items.forEach(video => {
-                if (video && !video.liveStreamingDetails) {
-                    const duration = moment.duration(video?.contentDetails?.duration)
+                        return null;
+                    }).filter(Boolean);
 
-                    if (duration.asMilliseconds() > 60 * 1000) {
-                        videos.push({
-                            id: video?.id,
-                            channelId: video?.snippet?.channelId,
-                            title: video?.snippet?.title,
-                            description: video?.snippet?.description,
-                            publishedAt: video?.snippet?.publishedAt,
-                            thumbnail: video?.snippet?.thumbnails?.default?.url,
-                            duration: duration / 1000,
-                            views: Number(video?.statistics?.viewCount) || 0,
-                            likes: Number(video?.statistics?.likeCount) || 0,
-                            comments: Number(video?.statistics?.commentCount) || 0
-                        })
-                    }
-                }
-            })
-
-            callback(null, videos)
-        }
-        else callback('Videos not found')
-    })
-    .catch(error => {
-        callback(error)
-    }) 
+                callback(null, videos);
+            } else {
+                callback('Videos not found');
+            }
+        })
+        .catch(error => {
+            callback(error);
+        });
 }
 
 module.exports = {
